cdf84bccd375be5ecd97c7709dd9638374d49a4f
andres.mera <feli041089@gmail.com>
2025-05-30 16:12:00 -0500

    code 02: Modification by the aggression of the new components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' } // Redirección para rutas no encontradas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuLyingDownComponent } from './modules/core/design-system/menu-lying-down/menu-lying-down.component';
+import { HomeComponent } from './modules/home/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuLyingDownComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
