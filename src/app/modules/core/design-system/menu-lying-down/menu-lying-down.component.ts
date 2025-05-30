1c0c8f23cabdad7c946392de0be2d9fb862931ab
andres.mera <feli041089@gmail.com>
2025-05-30 16:09:36 -0500

    code 02: Creating the app's lying menu.

---
+++ src/app/modules/core/design-system/menu-lying-down/menu-lying-down.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-lying-down',
+  templateUrl: './menu-lying-down.component.html',
+  styleUrl: './menu-lying-down.component.css'
+})
+export class MenuLyingDownComponent {
+
+}
