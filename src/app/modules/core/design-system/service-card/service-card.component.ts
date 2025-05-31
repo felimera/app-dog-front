79f161d2bb424c29eb77a5ddd422c00505b7e937
andres.mera <feli041089@gmail.com>
2025-05-31 12:13:32 -0500

    code 03: Creating the service card.

---
+++ src/app/modules/core/design-system/service-card/service-card.component.ts
@@ -0,0 +1,21 @@
+import { ServicePortfolio } from './../../models/service-portfolio';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-card',
+  templateUrl: './service-card.component.html',
+  styleUrl: './service-card.component.css'
+})
+export class ServiceCardComponent implements OnInit {
+
+  public servicePortfolios: ServicePortfolio[] = [];
+
+  ngOnInit(): void {
+    // Aquí normalmente harías una llamada a un servicio para obtener los productos
+    // Por ahora, usamos datos de ejemplo:
+    this.servicePortfolios = [
+      { id: 1, name: 'Mascotas', description: 'Administración de información básica de tus mascotas.' },
+      { id: 2, name: 'Revisión de tu mascota', description: 'Gestión de la revisión periódica de tu mascota.' }
+    ];
+  }
+}
