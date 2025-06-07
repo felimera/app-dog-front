90426bb4bc2b39cecc7552ba323e7aa355e7dd20
andres.mera <feli041089@gmail.com>
2025-06-07 14:29:06 -0500

    code 04: Modification of the app's card service.

--- src/app/modules/core/design-system/service-card/service-card.component.ts
+++ src/app/modules/core/design-system/service-card/service-card.component.ts
@@ -1,25 +1,10 @@
-import { ServicePortfolio } from './../../models/service-portfolio';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-service-card',
   templateUrl: './service-card.component.html',
   styleUrl: './service-card.component.css'
 })
-export class ServiceCardComponent implements OnInit {
+export class ServiceCardComponent {
 
-  public servicePortfolios: ServicePortfolio[] = [];
-
-  ngOnInit(): void {
-    // Aquí normalmente harías una llamada a un servicio para obtener los productos
-    // Por ahora, usamos datos de ejemplo:
-    this.servicePortfolios = [
-      { id: 1, name: 'Mascotas', description: 'Administración de información básica de tus mascotas.', patch: 'mascota' },
-      { id: 2, name: 'Revisión de tu mascota', description: 'Gestión de la revisión periódica de tu mascota.', patch: '' }
-    ];
-  }
-
-  public addressingTo(patch: string): void {
-    console.log('patch :', patch);
-  }
 }
