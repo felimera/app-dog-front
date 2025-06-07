da5caa07b2ac2280c85727ffc7c1753a36863d19
andres.mera <feli041089@gmail.com>
2025-06-07 14:25:09 -0500

    code 04: Rearrangement of components in the bar menu.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home/home.component';
 import { PetComponent } from './modules/pet/pet/pet.component';
 import { RevisionComponent } from './modules/revision/revision/revision.component';
+import { ServiceCardComponent } from './modules/core/design-system/service-card/service-card.component';
+import { ContactComponent } from './modules/contact/contact/contact.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
     component: HomeComponent,
     children: [
+      { path: 'tarjetas', component: ServiceCardComponent },
       { path: 'mascota', component: PetComponent },
       { path: 'revision', component: RevisionComponent }
     ]
   },
-  { path: '**', redirectTo: '' } // Redirección para rutas no encontradas
+  {
+    path: 'contacto',
+    component: ContactComponent
+  },
+  { path: '**', redirectTo: 'home/tarjetas' } // Redirección para rutas no encontradas
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { MenuLyingDownComponent } from './modules/core/design-system/menu-lying-down/menu-lying-down.component';
 import { HomeComponent } from './modules/home/home/home.component';
 import { ServiceCardComponent } from './modules/core/design-system/service-card/service-card.component';
+import { PetComponent } from './modules/pet/pet/pet.component';
+import { RevisionComponent } from './modules/revision/revision/revision.component';
+import { CardComponent } from './modules/core/design-system/card/card.component';
+import { ContactComponent } from './modules/contact/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuLyingDownComponent,
     HomeComponent,
-    ServiceCardComponent
+    ServiceCardComponent,
+    PetComponent,
+    RevisionComponent,
+    CardComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
