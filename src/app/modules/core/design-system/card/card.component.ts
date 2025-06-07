a40a0046c51af251793242026ad84cc42aea7c58
andres.mera <feli041089@gmail.com>
2025-06-07 14:38:47 -0500

    code 04: Logic to Redirect to the created component.

--- src/app/modules/core/design-system/card/card.component.ts
+++ src/app/modules/core/design-system/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServicePortfolio } from '../../models/service-portfolio';
+import { Router } from '@angular/router'; // Importa Router
 
 @Component({
   selector: 'app-card',
@@ -10,16 +11,18 @@ export class CardComponent implements OnInit {
 
   public servicePortfolios: ServicePortfolio[] = [];
 
+  constructor(private router: Router) { } // Inyecta Router
+
   ngOnInit(): void {
     // Aquí normalmente harías una llamada a un servicio para obtener los productos
     // Por ahora, usamos datos de ejemplo:
     this.servicePortfolios = [
-      { id: 1, name: 'Mascotas', description: 'Administración de información básica de tus mascotas.', patch: 'mascota' },
-      { id: 2, name: 'Revisión de tu mascota', description: 'Gestión de la revisión periódica de tu mascota.', patch: '' }
+      { id: 1, name: 'Mascotas', description: 'Administración de información básica de tus mascotas.', patch: 'home/mascota' },
+      { id: 2, name: 'Revisión de tu mascota', description: 'Gestión de la revisión periódica de tu mascota.', patch: 'home/revision' }
     ];
   }
 
   public addressingTo(patch: string): void {
-    console.log('patch :', patch);
+    this.router.navigate([patch]);
   }
 }
